4862d943e43a21dc35a6d86229da08fa23681706
Sakthi-Vikkraman <sakthi900145@gmail.com>
2022-09-22 19:29:09 +0530

    22/9/22

--- telephone/ITelephone.java
+++
@@ -1,9 +0,0 @@
-package telephone;
-
-public interface ITelephone{
-    void powerOn();
-    void dail(int phoneNumber);
-    void answer();
-    boolean callPhone(int phoneNumber);
-    boolean isRinging();
-}
